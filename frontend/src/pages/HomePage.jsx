9988d8d7bb257ff441d906236f7ea5e66c1df8cb
joup23 <joup23@naver.com>
2025-03-11 17:18:46 +0900

    회원가입 주석석

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -28,9 +28,9 @@ const HomePage = () => {
               </div>
             ) : (
               <div className="rounded-md shadow">
-                <Link to="/signup" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10">
+                {/* <Link to="/signup" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10">
                   시작하기
-                </Link>
+                </Link> */}
               </div>
             )}
           </div>
